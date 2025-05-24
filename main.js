4bddce944b12e0d3ab85dc25c2d91ffeb9c5a5ad
Eduardo Salermo <duduscotta@gmail.com>
2025-05-24 09:41:11 -0300

    finalizaçao

--- main.js
+++ main.js
@@ -6,22 +6,40 @@ function start() {
 }
 
 recognition.onresult = function(event) {
-    let content = event.result[0][0].transcript;
+    let content = event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML = content;
-    speak();
+    if(content =="Selfie."){
+        speak();    
+    }
+    
 }
 
 function speak(){
     let synth = window.speechSynthesis;
-    let speakData = document.getElementById("textbox").value;
+    let speakData = "tirando sua selfie em 5 segundos"
     let utterThis = new SpeechSynthesisUtterance(speakData);
     synth.speak(utterThis);
-    Webcam.attach("camera");
+    Webcam.attach(camera);
+    setTimeout(function(){
+        takeSnapshot();
+        save();
+    }, 5000);
 }
 
 Webcam.set({
     width: 360,
     height: 250,
     image_format: 'png',
-    jpeg_quality: 90
+    png_quality: 90
 });
+function takeSnapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="selfieImage" src="'+data_uri+'">';
+    });
+}
+function save(){
+    let link = document.getElementById("link");
+    let image = document.getElementById("selfieImage").src;
+    link.href = image;
+    link.click();
+}
